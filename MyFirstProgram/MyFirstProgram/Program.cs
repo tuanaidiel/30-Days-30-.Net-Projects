d3d63e6f3e896a10d8dec333fb274a16adb65fcc
tuanaidiel <tuanaidiel@gmail.com>
2025-01-09 23:18:12 +0800

    first commit

---
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to my website");
+            Console.Beep();      //beep sound
+
+            Console.Write("Hey");   //no extra line
+            Console.WriteLine(" You");
+            Console.WriteLine("\tTuan\nAidiel");
+
+            
+        }
+    }
+}
+
