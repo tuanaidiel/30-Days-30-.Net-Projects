9f7a54497d14468d4c7748da9800838f3fcd708e
Tuan Muhammad Aidiel <tuanaidiel@gmail.com>
2025-01-19 22:39:43 +0800

    Add files via upload

---
+++ P4 - Temperature Converter/P4 - Temperature Converter/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace P4___Temperature_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.cmbFrom = new System.Windows.Forms.ComboBox();
+            this.cmbTo = new System.Windows.Forms.ComboBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(83, 90);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(188, 22);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "Insert Any Temperature";
+            this.txtInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cmbFrom
+            // 
+            this.cmbFrom.FormattingEnabled = true;
+            this.cmbFrom.Location = new System.Drawing.Point(39, 157);
+            this.cmbFrom.Name = "cmbFrom";
+            this.cmbFrom.Size = new System.Drawing.Size(121, 24);
+            this.cmbFrom.TabIndex = 1;
+            this.cmbFrom.Text = "From";
+            // 
+            // cmbTo
+            // 
+            this.cmbTo.FormattingEnabled = true;
+            this.cmbTo.Location = new System.Drawing.Point(187, 157);
+            this.cmbTo.Name = "cmbTo";
+            this.cmbTo.Size = new System.Drawing.Size(121, 24);
+            this.cmbTo.TabIndex = 2;
+            this.cmbTo.Text = "To";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(151, 347);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(45, 16);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "Result\r\n";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(137, 229);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 68);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(346, 450);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.cmbTo);
+            this.Controls.Add(this.cmbFrom);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ComboBox cmbFrom;
+        private System.Windows.Forms.ComboBox cmbTo;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ P4 - Temperature Converter/P4 - Temperature Converter/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P4___Temperature_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadComboBoxes();
+        }
+
+        private void LoadComboBoxes()
+        {
+            cmbFrom.Items.AddRange(new string[] { "Celsius", "Fahrenheit", "Kelvin" });
+            cmbTo.Items.AddRange(new string[] { "Celsius", "Fahrenheit", "Kelvin" });
+            cmbFrom.SelectedIndex = 0;
+            cmbTo.SelectedIndex = 1;
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            double inputTemp;
+            if (!double.TryParse(txtInput.Text, out inputTemp))
+            {
+                MessageBox.Show("Please enter a valid number!", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string fromUnit = cmbFrom.SelectedItem.ToString();
+            string toUnit = cmbTo.SelectedItem.ToString();
+            double result = ConvertTemperature(inputTemp, fromUnit, toUnit);
+
+            lblResult.Text = $"Result: {result:F2} {toUnit}";
+
+        }
+
+        private double ConvertTemperature(double temp, string from, string to)
+        {
+            double tempInCelsius;
+
+            switch (from)
+            {
+                case "Fahrenheit":
+                    tempInCelsius = (temp - 32) * 5 / 9;
+                    break;
+                case "Kelvin":
+                    tempInCelsius = temp - 273;
+                    break;
+                default: // "Celsius"
+                    tempInCelsius = temp;
+                    break;
+            }
+
+            switch (to)
+            {
+                case "Fahrenheit":
+                    return (tempInCelsius * 9 / 5) + 32;
+                case "Kelvin":
+                    return tempInCelsius + 273;
+                default: // "Celsius"
+                    return tempInCelsius;
+            }
+        }
+
+    }
+
+}
