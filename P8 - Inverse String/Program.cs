6766d429bfef38280e7129e5566207d8c4cd8530
Tuan Muhammad Aidiel <tuanaidiel@gmail.com>
2025-05-08 11:06:22 +0800

    Add files via upload

---
+++ P8 - Inverse String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main (string[] args)
+    {
+        System.Console.WriteLine("Enter a string: ");
+        string input = Console.ReadLine();
+
+        string reversed = ReverseString(input);
+        System.Console.WriteLine("Reversed string: " + reversed);
+    }
+
+    public static string ReverseString(string str)
+    {
+        char[] chars = str.ToCharArray();
+        Array.Reverse(chars);
+        return new string (chars);
+    }
+}
