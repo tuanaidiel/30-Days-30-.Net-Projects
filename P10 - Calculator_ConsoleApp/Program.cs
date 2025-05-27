f46a5bbf77dad90521d5cc558d4cc840231a4c67
Tuan Muhammad Aidiel <tuanaidiel@gmail.com>
2025-05-27 23:19:12 +0800

    Add files via upload

---
+++ P10 - Calculator_ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Substract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Divide(int a, int b)
+    {
+        if (b == 0)
+        {
+            System.Console.WriteLine("Cannot divide by 0");
+            return 0;
+        }
+        return a / b;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Calculator calc = new Calculator();
+
+        System.Console.WriteLine("Add: " + calc.Add(10, 5));
+        System.Console.WriteLine("Substract: " + calc.Substract(10, 5));
+        System.Console.WriteLine("Multiply: " + calc.Multiply(10, 5));
+        System.Console.WriteLine("Divide: " + calc.Divide(10,5));
+    }
+}
---
+++ P11 - NameAge_ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        System.Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        System.Console.Write("Enter your age: ");
+        string age = Console.ReadLine();
+
+        System.Console.WriteLine($"Hello {name}, you are {age} years old.");
+    }
+}
